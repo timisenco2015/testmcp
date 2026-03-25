c86f82a1c2e211bf2f53159846623024ff73d80e
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2026-03-24 23:11:56 -0600

    Add analysis for SiteDocument

---
+++ analysisded/A/ __UICRET_CreateTarget.aspx.cs
@@ -0,0 +1,41 @@
+```json
+{
+  "filename": "__UICRET_CreateTarget.aspx.cs",
+  "found": true,
+  "summary": "The file is a class definition for creating a new user interface component that handles the creation of new individual records in a web-based system.",
+  "purpose": "To provide the user interface and backend functionality for entering a new individual, handling the associated form data, and enabling interactions with the web application.",
+  "entities": [
+    "UICRET_CreateTarget"
+  ],
+  "fields": [],
+  "actors": [],
+  "workflows": [],
+  "business_rules": [],
+  "validations": [
+    "ValidatorEnable(v_PID_7_DateTimeOfBirth, true)"
+  ],
+  "calculations": [],
+  "conditions": [],
+  "system_behavior": [],
+  "dependencies": [
+    "System",
+    "System.Collections",
+    "System.ComponentModel",
+    "System.Data",
+    "System.Drawing",
+    "System.Web",
+    "System.Web.SessionState",
+    "System.Web.UI",
+    "System.Web.UI.WebControls",
+    "System.Web.UI.HtmlControls",
+    "System.Reflection",
+    "System.Xml",
+    "System.IO",
+    "Sectorlynx.BaseViewControl",
+    "Sectorlynx.BaseCommon",
+    "System.Text.RegularExpressions"
+  ],
+  "exceptions": [],
+  "content_gaps": []
+}
+``` 
