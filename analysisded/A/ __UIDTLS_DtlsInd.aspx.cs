ca38e47e95526e5ccf989c0fa720a4615fe75e78
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2026-03-24 23:26:58 -0600

    Add analysis for SiteDocument

---
+++ analysisded/A/ __UIDTLS_DtlsInd.aspx.cs
@@ -0,0 +1,72 @@
+```json
+{
+  "filename": "__UIDTLS_DtlsInd.aspx.cs",
+  "found": true,
+  "summary": "The file defines a web user interface for viewing individual and non-client details, utilizing ASP.NET classes and components.",
+  "purpose": "The primary purpose of the file is to provide a user interface for displaying individual and non-client details with ASP.NET controls.",
+  "entities": [
+    "UIDTLS_DtlsInd",
+    "UIDTLS_DtlsNonClient"
+  ],
+  "fields": [
+    "WPEventIn_Label",
+    "WPLocIn_Label",
+    "l_PID_5_PatientName",
+    "l_PID_8_Sex",
+    "PID_8_Sex",
+    "l_PatientAKAs",
+    "PatientAKAs",
+    "l_PID_7_DateTimeOfBirth",
+    "PID_7_DateTimeOfBirth",
+    "l_Age",
+    "Age",
+    "l_PID_19_SsnNumberPatient",
+    "PID_19_SsnNumberPatient",
+    "l_PatientPHN",
+    "PatientPHN",
+    "l_Relationships",
+    "Relationships",
+    "b_Update",
+    "b_New",
+    "l_ClientDetails",
+    "PID_5_PatientName_XPN_GivenName",
+    "PID_5_PatientName_XPN_FamilylastName",
+    "PID_5_PatientName_XPN_MiddleInitialOrName",
+    "l_ID_HairColour",
+    "l_Name",
+    "b_Add",
+    "b_Transfer",
+    "WPEvent_Label",
+    "l_NonClientDetails",
+    "MsgBox",
+    "NID_2_NonClientContactPerson_XPN_GivenName",
+    "NID_2_NonClientContactPerson_XPN_FamilylastName",
+    "NID_2_NonClientContactPerson_XPN_MiddleInitialOrName",
+    "NID_1_NonClientGeneral_SLDB_Title",
+    "l_NID_1_NonClientGeneral_SLDB_Title",
+    "l_NID_1_NonClientGeneral_SLDB_Organization",
+    "NID_1_NonClientGeneral_SLDB_Organization",
+    "l_NID_2_NonClientLocation_PhoneNumberHome_XTN_Label",
+    "NID_2_NonClientLocation_PhoneNumberHome_XTN_AreaCode",
+    "NID_2_NonClientLocation_PhoneNumberHome_XTN_PhoneNumber",
+    "l_NID_2_NonClientLocation_PhoneNumberMobile_XTN_Label"
+  ],
+  "actors": [],
+  "workflows": [],
+  "business_rules": [],
+  "validations": [],
+  "calculations": [],
+  "conditions": [],
+  "system_behavior": [
+    "Update",
+    "Add",
+    "Transfer"
+  ],
+  "dependencies": [
+    "System.Web.UI.HtmlControls",
+    "System.Web.UI.WebControls"
+  ],
+  "exceptions": [],
+  "content_gaps": []
+}
+```
