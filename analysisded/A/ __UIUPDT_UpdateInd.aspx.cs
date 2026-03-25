82930813ee73cfa77eae62ecff025dc60e733c8f
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2026-03-24 23:27:23 -0600

    Add analysis for SiteDocument

---
+++ analysisded/A/ __UIUPDT_UpdateInd.aspx.cs
@@ -0,0 +1,45 @@
+```json
+{
+  "filename": "__UIUPDT_UpdateInd.aspx.cs",
+  "found": true,
+  "summary": "The UIUPDT_UpdateInd file provides a user interface for editing information for an individual, using ASP.NET Web Forms.",
+  "purpose": "This file implements a user interface to update individual data, including functionality to handle date input and personal information using web forms.",
+  "entities": [
+    "UIUPDT_UpdateInd"
+  ],
+  "fields": [
+    "PID_8_Sex",
+    "PID_19_SsnNumberPatient"
+  ],
+  "actors": [],
+  "workflows": [],
+  "business_rules": [],
+  "validations": [],
+  "calculations": [],
+  "conditions": [],
+  "system_behavior": [],
+  "dependencies": [
+    "System",
+    "System.Collections",
+    "System.ComponentModel",
+    "System.Data",
+    "System.Drawing",
+    "System.Web",
+    "System.Web.SessionState",
+    "System.Web.UI",
+    "System.Web.UI.WebControls",
+    "System.Web.UI.HtmlControls",
+    "System.Xml",
+    "System.Xml.XPath",
+    "System.IO",
+    "Sectorlynx.BaseViewControl",
+    "Sectorlynx.BaseCommon",
+    "System.Text.RegularExpressions"
+  ],
+  "exceptions": [],
+  "content_gaps": [
+    "No documents matched metadata->>'source' filter",
+    "No pageContent available"
+  ]
+}
+```
